e0f4a98401ffd52611083dbf43d13e8f99dc4dc3
Chaide Gampo <chaidegampo221@gmail.com>
2023-08-02 20:56:48 +0000

    refaire tous le fichier

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './component/App.jsx'
-import { Provider } from 'react-redux'
-import configureStore from './store/store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
-const store = configureStore() //fichier de configuration de store
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-<Provider store={store}>
     <React.StrictMode>
         <Router>
            <App /> 
         </Router>
     </React.StrictMode>
-</Provider>
-   
     
 )
 
